8e35dd27864c507c443491cb1d18bd4f0d44a720
talha <Talhaooa@gmail.com>
2024-06-03 11:28:32 +0200

    refactor: Passport init for auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
