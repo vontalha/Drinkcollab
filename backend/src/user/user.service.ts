4c74a9d67b0bf52bd5846fde696482ad7361117a
talha <Talhaooa@gmail.com>
2024-06-03 13:21:23 +0200

    feat: fetch user data

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private prisma: PrismaService){}
+
+    getUserByEmail = async (email: string) => {
+        const user = await this.prisma.user.findUnique({
+            where: {email}
+        })
+        return user
+    }
+    
+    getUserById = async (id: string | undefined) => {
+        const user = await this.prisma.user.findUnique({
+            where: {id}
+        })
+        return user
+    }
+    getUserByUsername = async (name: string) => {
+        const user = await this.prisma.user.findUnique({
+            where: {name}
+        })
+        return user
+    }
+    
+    getUserByLogin = async (login: string) => {
+        if (login.includes("@")){
+            return await this.getUserByEmail(login)
+        } 
+        return await this.getUserByLogin(login)
+    }
+}
