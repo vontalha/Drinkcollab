538264ecdc8c88ab48adcfa89305754fde983723
Felix <felix.matzdorf29@gmail.com>
2024-07-31 02:06:44 +0200

    login forwarding

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {HttpClientModule, provideHttpClient} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -11,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    //provideHttpClient()
     importProvidersFrom(HttpClientModule),
   ]
 };
